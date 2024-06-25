0e8b2815760cf8479038a25dfe16e60cff690db7
Ezenagu Chinemerem <chinemeremezenagu@gmail.com>
2024-06-25 22:42:30 +0000

    indentation

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 export default function returnHowManyArguments(...args) {
-=======
-export default function returnHowManyArguments() {
->>>>>>> 2b288f45def90c22d69c1662ff569acdd07c1410
   return args.length;
 }
